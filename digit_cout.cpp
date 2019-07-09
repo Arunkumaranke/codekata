791a1f55a3558d80e52a0d5c1a7dad09bb88697b
Joshua-Prakasam <40373946+Joshua-Prakasam@users.noreply.github.com>
2019-07-09 11:57:13 +0530

    Init

---
+++ digit_cout.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number;
+    int digit = 0;
+    
+    cin >> number;
+    
+    while( number )
+    {
+        digit++;
+        number /= 10;
+    }
+    
+    cout << digit;
+    
+    return 0;
+}
