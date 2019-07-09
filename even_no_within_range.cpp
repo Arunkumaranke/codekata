72818087f0cc53588cc73db4d5facd56bfcff3b3
Joshua-Prakasam <40373946+Joshua-Prakasam@users.noreply.github.com>
2019-07-09 14:59:37 +0530

    Init

---
+++ even_no_within_range.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int start, end, ele;
+    int flag = 0;
+    
+    cin >> start >> end;
+    ele = start + 1 ;
+    while(ele < end)
+    {
+        if( ele % 2 == 0 )
+        {
+            if(flag)
+                cout << " ";
+            cout << ele ;
+            flag = 1;
+        }
+        
+        ele++;
+    }
+    return 0;
+}
