7aa0f35815f2c5bdf093091c7101743a94e6ebd9
Joshua-Prakasam <40373946+Joshua-Prakasam@users.noreply.github.com>
2019-07-09 10:54:30 +0530

    Init

---
+++ leap_year.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    int inputYear;
+    
+    cin >> inputYear;
+    
+    if((inputYear % 4 == 0 && inputYear % 100 != 0) || (inputYear % 100 == 0  && inputYear % 400 == 0))
+        cout << "yes";
+    else
+        cout << "no";
+    
+    return 0;
+}
