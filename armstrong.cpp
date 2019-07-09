42a4a0b924412bcff83fd3f61893e2c194f5eff9
Joshua-Prakasam <40373946+Joshua-Prakasam@users.noreply.github.com>
2019-07-09 15:33:53 +0530

    Init

---
+++ armstrong.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num, temp;
+    int digits = 0, sum = 0;
+    
+    cin >> num;
+    
+    temp = num;
+    
+    while(temp)
+    {
+        temp /= 10;
+        digits++;
+    }
+    
+    temp = num;
+    
+    while(temp)
+    {
+        int digit = 1;
+        for(int times = 0; times < digits; times++)
+        {
+            digit *= temp%10;
+        }
+        sum += digit;
+        temp /= 10;
+    }
+    
+    cout << ((sum==num) ? "yes":"no");
+    
+    return 0;
+}
