4a1bb2e7a4c20ccd9683aa7951e7ebe349513d8a
Joshua-Prakasam <40373946+Joshua-Prakasam@users.noreply.github.com>
2019-07-09 10:57:35 +0530

    Init

--- leap_year.cpp
+++ leap_year.cpp
@@ -4,7 +4,6 @@ using namespace std;
 int main(){
     
     int inputYear;
-    
     cin >> inputYear;
     
     if((inputYear % 4 == 0 && inputYear % 100 != 0) || (inputYear % 100 == 0  && inputYear % 400 == 0))
