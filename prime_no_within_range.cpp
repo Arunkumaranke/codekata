0a0f7b4bf25171e66576f29eec0d2782c0358dab
Joshua-Prakasam <40373946+Joshua-Prakasam@users.noreply.github.com>
2019-07-09 15:10:53 +0530

    Init

---
+++ prime_no_within_range.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int start, end, ele;
+    int sp = 0;
+    
+    cin >> start >> end;
+    ele = start + 1 ;
+    while(ele < end)
+    {
+        int flag = 1;
+        
+        for(int divisor = 2; divisor <= ele/2; divisor++)
+        {
+            if(ele%divisor == 0)
+            {
+                flag =0;
+                break;
+            }
+        }
+        
+        if( flag )
+        {
+            if(sp)
+                cout << " ";
+            cout << ele ;
+            sp = 1;
+        }
+        
+        ele++;
+    }
+    
+    return 0;
+}
