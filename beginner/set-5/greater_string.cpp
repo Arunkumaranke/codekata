720e83c4b56cdd1311a104890d745781f494053a
Joshua-Prakasam <40373946+Joshua-Prakasam@users.noreply.github.com>
2019-08-06 11:57:17 +0530

    Init

--- beginner/set-5/greater_string.cpp
+++ beginner/set-5/greater_string.cpp
@@ -18,6 +18,6 @@ int main()
   string input1, input2;
   cin >> input1 >> input2;
   
-  (input1.length() >= input2.length()) ? cout << input1 : cout << input2;
+  (input1.length() > input2.length()) ? cout << input1 : cout << input2;
   return 0;
 }
