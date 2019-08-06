75128102c23e60cdadbfa34253759ca2061a6d76
Joshua-Prakasam <40373946+Joshua-Prakasam@users.noreply.github.com>
2019-08-06 12:42:37 +0530

    Init

---
+++ beginner/set-5/sum_of_two_numbers.cpp
@@ -0,0 +1,22 @@
+/*
+Given 2 numbers find their sum.
+Sample Testcase :
+INPUT
+5 5
+OUTPUT
+10
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int num1, num2;
+  
+  cin >> num1 >> num2;
+  cout << num1 + num2;
+  
+  return 0;
+}
