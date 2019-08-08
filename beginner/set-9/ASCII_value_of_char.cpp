3ac6cf119d8ae7c7f88752365e8a9aa20f7c365b
Joshua-Prakasam <40373946+Joshua-Prakasam@users.noreply.github.com>
2019-08-08 22:05:16 +0530

    Init

---
+++ beginner/set-9/ASCII_value_of_char.cpp
@@ -0,0 +1,23 @@
+/*
+Given a character Ch, print its ASCII value.
+Sample Testcase :
+INPUT
+a
+OUTPUT
+97
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  char ch;
+  
+  cin >> ch;
+  
+  cout << (int)ch;
+  
+  return 0;
+}
