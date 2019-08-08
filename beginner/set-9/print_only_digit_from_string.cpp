f59fc52bd2e1509ff962324385c88e446ed7ef21
Joshua-Prakasam <40373946+Joshua-Prakasam@users.noreply.github.com>
2019-08-08 22:16:57 +0530

    Init

---
+++ beginner/set-9/print_only_digit_from_string.cpp
@@ -0,0 +1,27 @@
+/*
+Given an alphanumeric string, print all the numbers in the string.
+Sample Testcase :
+INPUT
+5GUVI12
+OUTPUT
+512
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  string str;
+  
+  cin >> str;
+  
+  for( int ind = 0; str[ind] != 0; ind++ )
+  {
+      if( str[ind] >= '0' && str[ind] <= '9')
+        cout << str[ind];
+  }
+  
+  return 0;
+}
